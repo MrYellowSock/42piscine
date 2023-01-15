bbead0d818057f963ece8d9b33a6cbc48dafefa6
MrYellowSock <hahaunji@gmail.com>
2023-01-15 21:51:24 +0700

    ?

--- c00/myio.h
+++ c00/myio.h
@@ -16,26 +16,41 @@ int	m_str_len(char *str)
 	return (count);
 }
 
-void	m_put_nbr(int nb, int base, int padto)
+int	m_make_pos(int nb)
 {
-	int		max_index;
-	int		pad;
-	int		s_cur_digit;
-	int		s_cur_power;
-	char	digit;
-
 	if (nb < 0)
 	{
-		write(1, "-", 1);
 		nb = nb * -1;
+		return (1);
 	}
-	max_index = m_digitlen(nb, base);
-	pad = padto - max_index - 1;
+	return (0);
+}
+
+void	zero_pad(int nb, int base, int padto)
+{
+	int	pad;
+
+	pad = padto - m_digitlen(nb, base) - 1;
 	while (pad > 0)
 	{
 		write(1, "0", 1);
 		pad--;
 	}
+}
+
+void	m_put_nbr(int nb, int base, int padto)
+{
+	int		s_cur_digit;
+	int		s_cur_power;
+	char	digit;
+	int		max_index;
+
+	if (m_make_pos(nb))
+	{
+		write(1, "-", 1);
+	}
+	zero_pad(nb, base, padto);
+	max_index = m_digitlen(nb, base);
 	while (max_index >= 0)
 	{
 		s_cur_power = m_power(base, max_index);
