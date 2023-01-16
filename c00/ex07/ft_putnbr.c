16a2d1bdc4355858201f8cd6183eca5fa6c80d14
MrYellowSock <hahaunji@gmail.com>
2023-01-16 20:50:07 +0700

    all good except main

--- c00/ex07/ft_putnbr.c
+++ c00/ex07/ft_putnbr.c
@@ -6,79 +6,95 @@
 /*   By: skulkamt <skulkamt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 12:14:07 by skulkamt          #+#    #+#             */
-/*   Updated: 2023/01/16 13:39:14 by skulkamt         ###   ########.fr       */
+/*   Updated: 2023/01/16 20:48:39 by skulkamt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 
-int m_power(int base, int pow) {
-  int s_c;
-  int s_accumulate;
+int	m_power(int base, int pow)
+{
+	int	s_c;
+	int	s_accumulate;
 
-  s_accumulate = 1;
-  s_c = 0;
-  while (s_c < pow) {
-    s_accumulate *= base;
-    s_c = s_c + 1;
-  }
-  return (s_accumulate);
+	s_accumulate = 1;
+	s_c = 0;
+	while (s_c < pow)
+	{
+		s_accumulate *= base;
+		s_c = s_c + 1;
+	}
+	return (s_accumulate);
 }
 
-int m_digitlen(unsigned int nb, unsigned int base) {
-  int max_power;
+int	m_digitlen(unsigned int nb, unsigned int base)
+{
+	int	max_power;
 
-  max_power = 0;
-  while (nb / m_power(base, max_power) >= base) {
-    max_power = max_power + 1;
-  }
-  return (max_power);
+	max_power = 0;
+	while (nb / m_power(base, max_power) >= base)
+	{
+		max_power = max_power + 1;
+	}
+	return (max_power);
 }
 
-unsigned int remove_negative(int nb) {
-  unsigned int newnb = 0;
-  if (nb < 0) {
-    nb += 1;
-    nb *= -1;
-    newnb = (unsigned int)nb + 1;
-  } else {
-    newnb = nb;
-  }
-  return newnb;
+unsigned int	remove_negative(int nb)
+{
+	unsigned int	newnb;
+
+	newnb = 0;
+	if (nb < 0)
+	{
+		nb += 1;
+		nb *= -1;
+		newnb = (unsigned int)nb + 1;
+	}
+	else
+	{
+		newnb = nb;
+	}
+	return (newnb);
 }
 
-void m_put_nbr(int input, int base) {
-  int s_cur_digit;
-  int s_cur_power;
-  char digit;
-  int max_index;
-  unsigned int nb;
+void	ft_putnbr(int input)
+{
+	int				s_cur_digit;
+	int				s_cur_power;
+	char			digit;
+	int				max_index;
+	unsigned int	nb;
 
-  nb = remove_negative(input);
-  max_index = m_digitlen(nb, base);
-  if (input < 0) {
-    write(1, "-", 1);
-  }
-
-  while (max_index >= 0) {
-    s_cur_power = m_power(base, max_index);
-    s_cur_digit = nb / s_cur_power;
-    digit = '0' + s_cur_digit;
-    write(1, &digit, 1);
-    nb = nb - s_cur_power * s_cur_digit;
-    max_index = max_index - 1;
-  }
+	nb = remove_negative(input);
+	max_index = m_digitlen(nb, 10);
+	if (input < 0)
+	{
+		write(1, "-", 1);
+	}
+	while (max_index >= 0)
+	{
+		s_cur_power = m_power(10, max_index);
+		s_cur_digit = nb / s_cur_power;
+		digit = '0' + s_cur_digit;
+		write(1, &digit, 1);
+		nb = nb - s_cur_power * s_cur_digit;
+		max_index = max_index - 1;
+	}
 }
 
-void ft_putnbr(int nb) { m_put_nbr(nb, 10); }
+int	main(void)
+{
+	int	i;
 
-int main(void) {
-  for (int i = 0; i < 157; i++) {
-    ft_putnbr(i);
-    ft_putnbr(-i);
-    write(1, " ", 2);
-  }
-  ft_putnbr(-2147483648);
-  write(1, " ", 2);
-  ft_putnbr(2147483647);
-  return (0);
+	i = 0;
+	while (i < 157)
+	{
+		ft_putnbr(i);
+		ft_putnbr(-i);
+		write(1, " ", 2);
+		i++;
+	}
+	ft_putnbr(-2147483648);
+	write(1, " ", 2);
+	ft_putnbr(2147483647);
+	return (0);
 }
--- c00/ex08/ft_print_combn.c
+++ c00/ex08/ft_print_combn.c
@@ -6,52 +6,67 @@
 /*   By: skulkamt <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 14:52:54 by skulkamt          #+#    #+#             */
-/*   Updated: 2023/01/16 13:33:36 by skulkamt         ###   ########.fr       */
+/*   Updated: 2023/01/16 20:44:48 by skulkamt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <unistd.h>
 #define BUF_LEN 10
 
-void print_comb(char toprint[BUF_LEN], int cur_dex, int expected_len) {
-  if (cur_dex >= expected_len) {
-    return;
-  }
-  if (cur_dex <= 0) {
-    toprint[cur_dex] = '0';
-  } else {
-    toprint[cur_dex] = toprint[cur_dex - 1] + 1;
-  }
-
-  while (toprint[cur_dex] <= '9') {
-    print_comb(toprint, cur_dex + 1, expected_len);
-    if (cur_dex == expected_len - 1) {
-      if (!(toprint[0] == '0' &&
-            toprint[expected_len - 1] == '0' + expected_len - 1)) {
-        write(1, ", ", 2);
-      }
-      write(1, toprint, expected_len);
-    }
-    toprint[cur_dex]++;
-  }
+char	suitedvalue(char toprint[BUF_LEN], int cur_dex)
+{
+	if (cur_dex <= 0)
+	{
+		return ('0');
+	}
+	else
+	{
+		return (toprint[cur_dex - 1] + 1);
+	}
 }
 
-void ft_print_combn(int n) {
-
-  char toprint[BUF_LEN];
-  int i = 0;
-  while (i < BUF_LEN) {
-    toprint[i] = '0';
-    i++;
-  }
-  print_comb(toprint, 0, n);
+void	print_comb(char toprint[BUF_LEN], int cur_dex, int expected_len)
+{
+	if (cur_dex >= expected_len)
+	{
+		return ;
+	}
+	toprint[cur_dex] = suitedvalue(toprint, cur_dex);
+	while (toprint[cur_dex] <= '9')
+	{
+		print_comb(toprint, cur_dex + 1, expected_len);
+		if (cur_dex == expected_len - 1)
+		{
+			if (!(toprint[0] == '0' && toprint[expected_len - 1] == '0'
+					+ expected_len - 1))
+			{
+				write(1, ", ", 2);
+			}
+			write(1, toprint, expected_len);
+		}
+		toprint[cur_dex]++;
+	}
 }
 
-int main(void) {
-  ft_print_combn(1);
-  ft_print_combn(2);
-  ft_print_combn(3);
-  ft_print_combn(9);
-  return (0);
+void	ft_print_combn(int n)
+{
+	char	toprint[BUF_LEN];
+	int		i;
+
+	i = 0;
+	while (i < BUF_LEN)
+	{
+		toprint[i] = '0';
+		i++;
+	}
+	print_comb(toprint, 0, n);
+}
+
+int	main(void)
+{
+	ft_print_combn(1);
+	ft_print_combn(2);
+	ft_print_combn(3);
+	ft_print_combn(9);
+	return (0);
 }
