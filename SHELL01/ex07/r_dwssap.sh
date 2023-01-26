7312132dc127706dd25abac4a787bd48207f34a4
Suppanut Kulkamthorn <skulkamt@z2t13c2.42bangkok.com>
2023-01-26 17:03:55 +0700

    more fix

--- SHELL01/ex07/r_dwssap.sh
+++ SHELL01/ex07/r_dwssap.sh
@@ -1,3 +1,3 @@
 # echo "${FT_LINE1},${FT_LINE2}p" 
-cat /etc/passwd | sed '/^#/d' | sed -n 'p;n' |  sed 's/\(\S*\):.*/\1, /' | rev | sort -r | sed -n "${FT_LINE1},${FT_LINE2}p" | tr -d '\n' | sed 's/, $/./'
+cat /etc/passwd | sed '/^#/d' | sed -n 'p;n'  | rev |  sed 's/.*:\(\S*\)/\1/' | sort -r | sed -n "${FT_LINE1},${FT_LINE2}p" | tr '\n' ',' | sed 's/,$/./' | sed 's/,/, /g' 
 
