d4782129d561799fb584fa98ee0b5b66dca7f103
Suppanut Kulkamthorn <skulkamt@z2t13c2.42bangkok.com>
2023-01-28 20:21:59 +0700

    godspeed

--- c05/ex06/ft_is_prime.c
+++ c05/ex06/ft_is_prime.c
@@ -6,26 +6,43 @@
 /*   By: skulkamt <skulkamt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 19:57:42 by skulkamt          #+#    #+#             */
-/*   Updated: 2023/01/24 19:57:58 by skulkamt         ###   ########.fr       */
+/*   Updated: 2023/01/28 20:19:55 by skulkamt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_is_prime(int nb)
+int	ft_sqrt(int nb)
 {
-	int	i;
+	int	x0;
+	int	x1;
 
-	i = 2;
-	while (i < nb)
-	{
-		if (nb % i == 0)
-		{
-			return (0);
-		}
-		i++;
-	}
 	if (nb <= 1)
+		return (0);
+	x0 = nb / 2;
+	x1 = (x0 + nb / x0) / 2;
+	while (x1 < x0)
+	{
+		x0 = x1;
+		x1 = (x0 + nb / x0) / 2;
+	}
+	if (x0 * x0 == nb)
+	{
+		return (x0);
+	}
+	else
 	{
 		return (0);
 	}
-	return (1);
+	return (x0);
+}
+
+int ft_is_prime(int number)
+{
+	if (number < 2) return 0;
+	if (number % 2 == 0) return (number == 2);
+	int root = (int)ft_sqrt((double)number);
+	for (int i = 3; i <= root; i += 2)
+	{
+		if (number % i == 0) return 0;
+	}
+	return 1;
 }
--- c05/ex07/ft_find_next_prime.c
+++ c05/ex07/ft_find_next_prime.c
@@ -6,25 +6,45 @@
 /*   By: skulkamt <skulkamt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 19:57:47 by skulkamt          #+#    #+#             */
-/*   Updated: 2023/01/28 19:12:58 by skulkamt         ###   ########.fr       */
+/*   Updated: 2023/01/28 20:19:19 by skulkamt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_is_prime(int nb, int i)
+int	ft_sqrt(int nb)
 {
-	while (i < nb)
-	{
-		if (nb % i == 0)
-		{
-			return (0);
-		}
-		i++;
-	}
+	int	x0;
+	int	x1;
+
 	if (nb <= 1)
+		return (0);
+	x0 = nb / 2;
+	x1 = (x0 + nb / x0) / 2;
+	while (x1 < x0)
+	{
+		x0 = x1;
+		x1 = (x0 + nb / x0) / 2;
+	}
+	if (x0 * x0 == nb)
+	{
+		return (x0);
+	}
+	else
 	{
 		return (0);
 	}
-	return (1);
+	return (x0);
+}
+
+int isprime(int number)
+{
+	if (number < 2) return 0;
+	if (number % 2 == 0) return (number == 2);
+	int root = (int)ft_sqrt((double)number);
+	for (int i = 3; i <= root; i += 2)
+	{
+		if (number % i == 0) return 0;
+	}
+	return 1;
 }
 
 int	ft_find_next_prime(int nb)
@@ -36,7 +56,7 @@ int	ft_find_next_prime(int nb)
 	{
 		ans = 2;
 	}
-	while (!ft_is_prime(ans, 2) && ans < 2147483647)
+	while (!isprime(ans) && ans < 2147483647)
 	{
 		ans++;
 	}
