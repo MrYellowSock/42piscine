9e08020a784fc0ff079b8ee41c8f1e1e0a522383
Suppanut Kulkamthorn <skulkamt@z2t13c2.42bangkok.com>
2023-01-29 14:48:21 +0700

    not broke not fix

--- c05/ex06/ft_is_prime.c
+++ c05/ex06/ft_is_prime.c
@@ -6,32 +6,26 @@
 /*   By: skulkamt <skulkamt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 19:57:42 by skulkamt          #+#    #+#             */
-/*   Updated: 2023/01/29 13:19:56 by skulkamt         ###   ########.fr       */
+/*   Updated: 2023/01/29 14:48:08 by skulkamt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int    close_sqrt(int nb)
+int	ft_is_prime(int nb)
 {
-    int    i;
+	int	i;
 
-    i = 1;
-    if (nb == 1)
-        return (nb);
-    while (i <= nb / i)
-    {
-        i++;
-    }
-    return (i);
-}
-
-int ft_is_prime(int number)
-{
-	if (number < 2) return 0;
-	if (number % 2 == 0) return (number == 2);
-	int root = (int)close_sqrt((double)number);
-	for (int i = 3; i <= root; i += 2)
+	i = 2;
+	while (i < nb)
 	{
-		if (number % i == 0) return 0;
+		if (nb % i == 0)
+		{
+			return (0);
+		}
+		i++;
 	}
-	return 1;
+	if (nb <= 1)
+	{
+		return (0);
+	}
+	return (1);
 }
